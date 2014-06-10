61b331279ae626ab2a260eb879d9341ea378fe35
Bernhard Rode <mail@bernhardrode.de>
2014-06-10 15:33:51 +0200

    Many many Social Integration Meta Tags

--- templates/head.php
+++ templates/head.php
@@ -1,38 +1,72 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
-  <?php if (is_single() || is_page() ) { ?>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <meta property="og:title" content="<?php utf8_decode(single_post_title());?>"/>
-  <?php if ( has_post_thumbnail()) {
-    $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large'); ?>
-    <meta property="og:image" content="<?php echo $large_image_url['0'] ?>"/>
-  <?php }
-        else{ ?>
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/rodereisen.png"/>
-  <?php } ?>
-  <meta property="og:url" content="<?php echo get_permalink();?>?utm_source=social_media"/>
-  <?php } ?> 
-  
   <?php 
-    global $post;
-    $description = "Sie suchen Ihren nächsten Traumurlaub ✓ Dann sind wir Ihr Partner  Über 30 Jahre Erfahrungen sprechen für sich.";
-    if (is_single()) {
-      $description = strip_tags(get_the_excerpt($post->ID));
-    } 
+    $title = get_the_title('|', true, 'right');
+    $image = get_template_directory_uri() . "/assets/img/rodereisen.png";
   ?>
-  <meta property="og:description" content="<?php echo $description; ?>"/>
-
-  <meta property="og:site_name" content="Reisebüro Rode GmbH"/>
-  <meta property="og:type" content="website"/>
+  <?php if (is_single() || is_page() ) {
+    $title = utf8_decode(single_post_title());
+    if ( has_post_thumbnail()) {
+      $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');
+      $image = $large_image_url['0'];
+    }
   
+  $url = get_permalink() . "?utm_source=social_media";
+  global $post;
+  $description = "Sie suchen Ihren nächsten Traumurlaub ✓ Dann sind wir Ihr Partner  Über 30 Jahre Erfahrungen sprechen für sich.";
+  $url = get_permalink( $post->ID );
+  if (is_single()) {
+    $description = strip_tags(get_the_excerpt($post->ID));
+  } 
+  ?>
+  
+  <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="description" content="<?php echo $description; ?>" />
   
+  <!-- BEGIN OF META TAGS -->
+  <!-- Global Meta Tags -->
+  <meta property="place:location:latitude" content="49.037170"/>
+  <meta property="place:location:longitude" content="9.316270"/>
+  <meta property="business:contact_data:street_address" content="Oberstenfelderstr. 16"/>
+  <meta property="business:contact_data:locality" content="Beilstein"/>
+  <meta property="business:contact_data:postal_code" content="71717"/>
+  <meta property="business:contact_data:country_name" content="Germany"/>
+  <meta property="business:contact_data:email" content="info@rodereisen.de"/>
+  <meta property="business:contact_data:phone_number" content="+49 706294990"/>
+  <meta property="business:contact_data:website" content="http://www.rodereisen.de"/>
+  <!-- Google+ Meta Tags -->
   <link rel="publisher" href="https://plus.google.com/+rodereisende"/>
+  <meta itemprop="name" content="<?php wp_title('|', true, 'right'); ?>"/>
+  <meta itemprop="description" content="<?php echo $description; ?>"/>
+  <meta itemprop="image" content="<?php echo $image; ?>"/>
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@reisebuerorode">
+  <meta name="twitter:title" content="<?php wp_title('|', true, 'right'); ?>">
+  <meta name="twitter:description" content="<?php echo $description; ?>">
+  <meta name="twitter:creator" content="@reisebuerorode">
+  <meta name="twitter:image:src" content="<?php echo $image; ?>">
+  <meta name="twitter:domain" content="rodereisen.de">
+  <!-- Facebook Meta Tags -->
+  <meta property="og:type" content="profile"/> 
+  <meta property="profile:first_name" content="Reisebüro"/> 
+  <meta property="profile:last_name" content="Rode"/>
+  <meta property="profile:username" content=""/>
+  <meta property="og:title" content="<?php wp_title('|', true, 'right'); ?>"/>
+  <meta property="og:description" content="<?php echo $description; ?>"/>
+  <meta property="og:image" content="<?php echo $image; ?>"/>
+  <meta property="og:url" content="<?php echo $url; ?>"/>
+  <meta property="og:site_name" content="<?php wp_title('|', true, 'right'); ?>"/>
+  <meta property="og:see_also" content="http://www.rodereisen.de"/>
+  <meta property="fb:admins" content=""/>
+  <!-- END OF META TAGS -->
+  
+  
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/rodereisen.png" type="image/png" />
   <?php wp_head(); ?>
   <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
