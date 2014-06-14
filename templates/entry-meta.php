e470c3ae81f23754adeaeb7251f135647721f5a0
Bernhard Rode <mail@bernhardrode.de>
2014-06-14 14:17:18 +0200

    Updated

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -3,11 +3,14 @@
   <time class="published" datetime="<?php echo get_the_time('c'); ?>">
     <?php echo get_the_date(); ?>
   </time>
+  <time class="updated" datetime="<?php echo get_the_time('c'); ?>" hidden>
+    <?php echo get_the_date(); ?>
+  </time>
 
   <p class="byline author vcard">
     <?php echo __('By', 'roots'); ?>
     <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">
       <?php echo get_the_author(); ?>
-    </a> - <a href="<?php echo the_author_meta('url'); ?>?rel=author" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>"><i class="fa fa-google-plus"></i></a>
+    </a> - <a href="<?php echo the_author_meta('url'); ?>?rel=author" rel="author-Link" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>"><i class="fa fa-google-plus"></i></a>
   </p>
 </div>
