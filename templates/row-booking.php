1390d48cfd895d0f9dd8b7f2f41a317ce05527c4
Bernhard Rode <mail@bernhardrode.de>
2014-06-14 12:14:30 +0200

    Frontpage

--- templates/row-booking.php
+++ templates/row-booking.php
@@ -1,41 +1,35 @@
-<?php
-  if (  is_user_logged_in() ) {
-?>
-  <div class="hidden-xs">
-    <div class="row">
-      <div class="col col-lg-12">
-        <div class="page-header">
-          <h1 class="gradient-red">Online Buchen</h1>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col col-xs-4">
-        <a href="/online-buchen">
-          <div class="box text-center">
-            <i class="fa fa-suitcase fa-5x gradient"></i>
-            <h1 class="gradient">Pauschal</h1>
-          </div>
-        </a>
-      </div>
-      <div class="col col-xs-4">
-        <a href="/online-buchen/nur-flug">
-          <div class="box text-center">
-            <i class="fa fa-plane fa-5x gradient"></i>
-            <h1 class="gradient">Flüge</h1>
-          </div>
-        </a>
-      </div>
-      <div class="col col-xs-4">
-        <a href="/online-buchen/nur-hotel">
-          <div class="box text-center">
-            <i class="fa fa-institution fa-5x gradient"></i>
-            <h1 class="gradient">Hotels</h1>
-          </div>
-        </a>
+<div class="hidden-xs">
+  <div class="row">
+    <div class="col col-lg-12">
+      <div class="page-header">
+        <h1 class="gradient-red">Online Buchen</h1>
       </div>
     </div>
   </div>
-<?php
- }
-?>
+  <div class="row">
+    <div class="col col-xs-4">
+      <a href="/online-buchen">
+        <div class="box text-center">
+          <i class="fa fa-suitcase fa-5x gradient"></i>
+          <h1 class="gradient">Pauschal</h1>
+        </div>
+      </a>
+    </div>
+    <div class="col col-xs-4">
+      <a href="/online-buchen/nur-flug">
+        <div class="box text-center">
+          <i class="fa fa-plane fa-5x gradient"></i>
+          <h1 class="gradient">Flüge</h1>
+        </div>
+      </a>
+    </div>
+    <div class="col col-xs-4">
+      <a href="/online-buchen/nur-hotel">
+        <div class="box text-center">
+          <i class="fa fa-institution fa-5x gradient"></i>
+          <h1 class="gradient">Hotels</h1>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
--- templates/row-open.php
+++ templates/row-open.php
@@ -1,69 +1,34 @@
 <div class="margin-top-20 row-open">
   <div class="row">
-    <div class="col col-xs-6 text-center">
+    <div class="col col-sm-4 text-center">
       <div class="box">
-        <i class="fa fa-clock-o fa-5x gradient"></i>
-        <?php
-          $times = array(
-              'mon' => '9:00 AM - 06:00 PM',
-              'tue' => '9:00 AM - 06:00 PM',
-              'wed' => '9:00 AM - 06:00 PM',
-              'thu' => '9:00 AM - 06:00 PM',
-              'fri' => '9:00 AM - 06:00 PM',
-              'sat' => '9:00 AM - 01:00 PM',
-              'sun' => 'closed'
-          );
-
-          function compileHours($times, $timestamp) {
-              $times = $times[strtolower(date('D',$timestamp))];
-              if(!strpos($times, '-')) return array();
-              $hours = explode(",", $times);
-              $hours = array_map('explode', array_pad(array(),count($hours),'-'), $hours);
-              $hours = array_map('array_map', array_pad(array(),count($hours),'strtotime'), $hours, array_pad(array(),count($hours),array_pad(array(),2,$timestamp)));
-              end($hours);
-              if ($hours[key($hours)][0] > $hours[key($hours)][1]) $hours[key($hours)][1] = strtotime('+1 day', $hours[key($hours)][1]);
-              return $hours;
-          }
-
-          function isOpen($now, $times) {
-              $open = 0; // time until closing in seconds or 0 if closed
-              // merge opening hours of today and the day before
-              $hours = array_merge(compileHours($times, strtotime('yesterday',$now)),compileHours($times, $now));
-
-              foreach ($hours as $h) {
-                  if ($now >= $h[0] and $now < $h[1]) {
-                      $open = $h[1] - $now;
-                      return $open;
-                  }
-              }
-              return $open;
-          }
-
-          $now = strtotime('7:59pm');
-          $open = isOpen($now, $times);
-
-          if ($open == 0) {
-              ?>
-              <h2 class="gradient-red">Wir haben derzeit geschlossen.</h2>
-              <?php
-          } else {
-              //echo "Is open. Will close in ".ceil($open/60)." minutes";
-              ?>
-              <h2 class="gradient">Wir haben für Sie Geöffnet.</h2>
-              <?php
-          }
-        ?>
+        <i class="fa fa-info fa-5x gradient"></i>
+        <ul class="list-unstyled">
+          <li><strong>Beilstein</strong><br><a href="tel:0049706294990">+49 (0)7062 94990</a></li>
+          <li><strong>Steinheim</strong><br><a href="tel:0049714481550">+49 (0)7144 81550</a></li>
+          <li><strong>Mail</strong><br><a href="mailto:info@rodereisen.de">info@rodereisen.de</a></li>
+        </ul>
       </div>
     </div>
-    <div class="col col-xs-6 text-center">
+    <div class="col col-sm-4 text-center">
       <div class="box">
-        <i class="fa fa-th-list fa-5x gradient"></i>
-        <h2 class="gradient hidden-xs">Öffnungszeiten</h2>
+        <i class="fa fa-clock-o fa-5x gradient"></i>
         <ul class="list-unstyled">
-          <li><strong>Mo bis Fr </strong>9.00 Uhr - 18.00 Uhr</li>
+          <li><strong>Mo bis Fr </strong><br>9.00 Uhr - 18.00 Uhr</li>
           <li><strong>Sa: </strong>9.00 Uhr - 13.00 Uhr</li>
           <li><strong>So: </strong>geschlossen</li>
         </ul>
+      </div>
+    </div>
+    <div class="col col-sm-4 text-center">
+      <div class="box">
+        <i class="fa fa-heart-o fa-5x gradient"></i>
+        <h3>
+          Immer aktuell Informiert
+        </h3>
+        <a href="https://plus.google.com/+rodereisende/posts" target="_blank" class="btn btn-lg btn-block btn-danger">
+          Unsere <i class="fa fa-google-plus-square"></i> Seite
+        </a>
       </div>
     </div>
   </div>
