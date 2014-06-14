7194051bb86a6fc1fc34a7295768f3f161d366d9
Bernhard Rode <mail@bernhardrode.de>
2014-06-14 14:19:45 +0200

    Updated Author Link

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -9,8 +9,8 @@
 
   <p class="byline author vcard">
     <?php echo __('By', 'roots'); ?>
-    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn">
+    <a href="<?php echo the_author_meta('url'); ?>?rel=author" rel="author-Link" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>">
       <?php echo get_the_author(); ?>
-    </a> - <a href="<?php echo the_author_meta('url'); ?>?rel=author" rel="author-Link" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>"><i class="fa fa-google-plus"></i></a>
+    </a>
   </p>
 </div>
