a2d90c0fb3b2cef7c41a4985d9adf9000e3bd713
Bernhard Rode <mail@bernhardrode.de>
2014-06-14 14:23:18 +0200

    typofix

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -9,7 +9,7 @@
 
   <p class="byline author vcard">
     <?php echo __('By', 'roots'); ?>
-    <a href="<?php echo the_author_meta('url'); ?>?rel=author" rel="author-Link" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>" classn="fn">
+    <a href="<?php echo the_author_meta('url'); ?>?rel=author" rel="author-Link" target="_blank" title="Google Plus Profile von <?php echo get_the_author(); ?>" class="fn">
       <?php echo get_the_author(); ?>
     </a>
   </p>
