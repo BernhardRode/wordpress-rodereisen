910f6c53d2ca9f0a35f69828359b57190b29fad3
Bernhard Rode <mail@bernhardrode.de>
2014-06-16 10:18:26 +0200

    Add box around content

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="box">
   <?php while (have_posts()) : the_post(); ?>
     <?php the_content(); ?>
     <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
