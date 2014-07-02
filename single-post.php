d1396be80dc91c697a23d1c3a08108f33a9942da
Bernhard Rode <mail@bernhardrode.de>
2014-07-02 15:03:08 +0200

    Informationen für Blog Posts entfernen

--- single-post.php
+++ single-post.php
@@ -7,10 +7,8 @@
             <div class="text-center">
               <?php the_post_thumbnail('thumbnail', array('class' => 'img-circle img-shadow')); ?>
               <span itemprop="image" hidden>http://www.rodereisen.de<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?></span>
-              <div class="hidden-xs margin-top-20">
-                <a class="btn btn-danger btn-lg btn-block" href="<?php echo $kontakt_url; ?>">Informationen anfordern</a>
-              </div>
               <?php get_template_part('templates/entry-meta'); ?>
+
             </div>
           </div>
           <div class="col col-md-8">
