a59a3b80773c7719772dda4fad3f067b854d5e44
Bernhard Rode <mail@bernhardrode.de>
2014-07-03 18:16:26 +0200

    Alternate responsive images solution

--- lib/custom.php
+++ lib/custom.php
@@ -284,3 +284,27 @@ function remove_width_attribute( $html ) {
    return $html;
 }
 
+// add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+// add_shortcode('caption', 'fixed_img_caption_shortcode');
+// function fixed_img_caption_shortcode($attr, $content = null) {
+//   if ( ! isset( $attr['caption'] ) ) {
+//     if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+//       $content = $matches[1];
+//       $attr['caption'] = trim( $matches[2] );
+//     }
+//   }
+//   $output = apply_filters('img_caption_shortcode', '', $attr, $content);
+//   if ( $output != '' )
+//     return $output;
+//   extract(shortcode_atts(array(
+//     'id'  => '',
+//     'align' => 'alignnone',
+//     'width' => '',
+//     'caption' => ''
+//   ), $attr));
+//   if ( 1 > (int) $width || empty($caption) )
+//     return $content;
+//   if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+//   return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" >' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+// }
+
