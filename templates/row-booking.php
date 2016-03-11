4ff9344c6ceb1a4604819a13d100fb0d461466e2
Bernhard Rode <bernhard.rode@dmc.de>
2016-03-11 10:13:40 +0100

    feature(Booking): all in one line with same height

--- templates/row-booking.php
+++ templates/row-booking.php
@@ -7,38 +7,40 @@
         </div>
     </div>
     <div class="row">
-        <div class="col col-xs-4">
+        <div class="col col-xs-3">
             <a href="/online-buchen">
-                <div class="box text-center">
+                <div class="box booking-box text-center">
                     <i class="fa fa-suitcase fa-5x gradient"></i>
 
                     <h1 class="gradient">Pauschal</h1>
                 </div>
             </a>
         </div>
-        <div class="col col-xs-4">
+        <div class="col col-xs-3">
             <a href="/online-buchen/nur-flug">
-                <div class="box text-center">
+                <div class="box booking-box text-center">
                     <i class="fa fa-plane fa-5x gradient"></i>
 
                     <h1 class="gradient">Flüge</h1>
                 </div>
             </a>
         </div>
-        <div class="col col-xs-4">
+        <div class="col col-xs-3">
             <a href="/online-buchen/nur-hotel">
-                <div class="box text-center">
+                <div class="box booking-box text-center">
                     <i class="fa fa-institution fa-5x gradient"></i>
 
                     <h1 class="gradient">Hotels</h1>
                 </div>
             </a>
         </div>
-    </div>
-    <div class="row">
-        <div class="col col-xs-4">
+        <div class="col col-xs-3">
             <a href="http://www.fitreisen.de/wellness.html?agenturnr=10205511" target="_blank">
-                <img src="/app/themes/rr-press/assets/img/banner/fti/01-wellnesswochenende/01a_FIT-Reisen_WellnessWE_220Quellenhotel_336x280.gif" class="img-responsive" style="min-width:100%;">
+                <div class="box booking-box text-center">
+                    <img
+                        src="/app/themes/rr-press/assets/img/banner/fti/01-wellnesswochenende/01a_FIT-Reisen_WellnessWE_220Quellenhotel_336x280.gif"
+                        class="img-responsive" style="min-width:100%;">
+                </div>
             </a>
         </div>
     </div>
