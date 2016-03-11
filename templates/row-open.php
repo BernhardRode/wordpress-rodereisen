56d994ca73719c53994ffad45367e3dfaebf7441
Bernhard Rode <bernhard.rode@dmc.de>
2016-03-11 09:32:10 +0100

    (PHP): reformat code, change order of facebook & g+

--- templates/row-open.php
+++ templates/row-open.php
@@ -1,72 +1,73 @@
 <div class="margin-top-20 row-open non-printable">
-  <div class="row">
-    <div class="col col-sm-4 text-center">
-      <div class="box">
-        <i class="fa fa-info fa-5x gradient"></i>
-        <ul class="list-unstyled hidden-xs">
-          <li>
-            <strong>Beilstein</strong><br>
-            <a href="tel:0049706294990">+49 (0)7062 94990</a>
-          </li>
-          <li>
-            <strong>Steinheim</strong><br><a href="tel:0049714481550">+49 (0)7144 81550</a>
-          </li>
-          <li>
-            <strong>Mail</strong><br><a href="mailto:info@rodereisen.de">info@rodereisen.de</a>
-          </li>
-        </ul>
-        <ul class="list-unstyled visible-xs">
-          <li>
-            <a href="tel:0049706294990" class="btn btn-success btn-block">
-              <i class="fa fa-phone"></i> Beilstein
-            </a>
-          </li>
-          <li>
-            <a href="tel:0049714481550" class="btn btn-success btn-block">
-              <i class="fa fa-phone"></i> Steinheim
-            </a>
-          </li>
-          <li>
-            <a href="mailto:info@rodereisen.de" class="btn btn-success btn-block">
-              <i class="fa fa-envelope"></i> info@rodereisen.de
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col col-sm-4 text-center">
-      <div class="box">
-        <i class="fa fa-clock-o fa-5x gradient"></i>
-        <ul class="list-unstyled" itemscope itemtype="http://schema.org/LocalBusiness">
-          <li>
-            <h3 class="visible-xs"><strong>Mo bis Fr </strong><br>9.00 Uhr - 18.00 Uhr</h3>
-            <span class="hidden-xs"><strong>Mo bis Fr </strong><br>9.00 Uhr - 18.00 Uhr</span>
-            <meta itemprop="openingHours" content="Mo-Fr 09:00-18:00">
-          </li>
-          <li>
-            <h3 class="visible-xs"><strong>Sa: </strong>9.00 Uhr - 13.00 Uhr</h3>
-            <span class="hidden-xs"><strong>Sa: </strong>9.00 Uhr - 13.00 Uhr</span>
-            <meta itemprop="openingHours" content="Sa 09:00-13:00">
-          </li>
-          <li>
-            <h3 class="visible-xs"><strong>So: </strong>geschlossen</h3>
-            <span class="hidden-xs"><strong>So: </strong>geschlossen</span>
-          </li>
+    <div class="row">
+        <div class="col col-sm-4 text-center">
+            <div class="box">
+                <i class="fa fa-info fa-5x gradient"></i>
+                <ul class="list-unstyled hidden-xs">
+                    <li>
+                        <strong>Beilstein</strong><br>
+                        <a href="tel:0049706294990">+49 (0)7062 94990</a>
+                    </li>
+                    <li>
+                        <strong>Steinheim</strong><br><a href="tel:0049714481550">+49 (0)7144 81550</a>
+                    </li>
+                    <li>
+                        <strong>Mail</strong><br><a href="mailto:info@rodereisen.de">info@rodereisen.de</a>
+                    </li>
+                </ul>
+                <ul class="list-unstyled visible-xs">
+                    <li>
+                        <a href="tel:0049706294990" class="btn btn-success btn-block">
+                            <i class="fa fa-phone"></i> Beilstein
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tel:0049714481550" class="btn btn-success btn-block">
+                            <i class="fa fa-phone"></i> Steinheim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:info@rodereisen.de" class="btn btn-success btn-block">
+                            <i class="fa fa-envelope"></i> info@rodereisen.de
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col col-sm-4 text-center">
+            <div class="box">
+                <i class="fa fa-clock-o fa-5x gradient"></i>
+                <ul class="list-unstyled" itemscope itemtype="http://schema.org/LocalBusiness">
+                    <li>
+                        <h3 class="visible-xs"><strong>Mo bis Fr </strong><br>9.00 Uhr - 18.00 Uhr</h3>
+                        <span class="hidden-xs"><strong>Mo bis Fr </strong><br>9.00 Uhr - 18.00 Uhr</span>
+                        <meta itemprop="openingHours" content="Mo-Fr 09:00-18:00">
+                    </li>
+                    <li>
+                        <h3 class="visible-xs"><strong>Sa: </strong>9.00 Uhr - 13.00 Uhr</h3>
+                        <span class="hidden-xs"><strong>Sa: </strong>9.00 Uhr - 13.00 Uhr</span>
+                        <meta itemprop="openingHours" content="Sa 09:00-13:00">
+                    </li>
+                    <li>
+                        <h3 class="visible-xs"><strong>So: </strong>geschlossen</h3>
+                        <span class="hidden-xs"><strong>So: </strong>geschlossen</span>
+                    </li>
 
-        </ul>
-      </div>
+                </ul>
+            </div>
+        </div>
+        <div class="col col-sm-4 text-center">
+            <div class="box">
+                <i class="fa fa-heart-o fa-5x gradient"></i>
+                <a href="https://www.facebook.com/ReisebueroRode" target="_blank"
+                   class="btn btn-lg btn-block btn-primary">
+                    Unsere <i class="fa fa-facebook-square"></i> Seite
+                </a>
+                <a href="https://plus.google.com/+rodereisende/posts" target="_blank"
+                   class="btn btn-lg btn-block btn-danger">
+                    Unsere <i class="fa fa-google-plus-square"></i> Seite
+                </a>
+            </div>
+        </div>
     </div>
-    <div class="col col-sm-4 text-center">
-      <div class="box">
-        <i class="fa fa-heart-o fa-5x gradient"></i>
-        <a href="https://plus.google.com/+rodereisende/posts" target="_blank" class="btn btn-lg btn-block btn-danger">
-          Unsere <i class="fa fa-google-plus-square"></i> Seite
-        </a>
-        <a href="https://www.facebook.com/ReisebueroRode" target="_blank" class="btn btn-lg btn-block btn-primary">
-          Unsere <i class="fa fa-facebook-square"></i> Seite
-        </a>
-      </div>
-    </div>
-  </div>
-  </div>
 </div>
