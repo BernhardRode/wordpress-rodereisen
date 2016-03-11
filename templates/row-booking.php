8622c00f07fb6ef35cb62a7607cd99f77d7c67d5
Bernhard Rode <bernhard.rode@dmc.de>
2016-03-11 09:32:34 +0100

    feature(Booking): add wellness banner from fitreisen

--- templates/row-booking.php
+++ templates/row-booking.php
@@ -1,35 +1,45 @@
 <div class="hidden-xs non-printable">
-  <div class="row">
-    <div class="col col-lg-12">
-      <div class="page-header">
-        <h1 class="gradient-red">Online Buchen</h1>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col col-xs-4">
-      <a href="/online-buchen">
-        <div class="box text-center">
-          <i class="fa fa-suitcase fa-5x gradient"></i>
-          <h1 class="gradient">Pauschal</h1>
+    <div class="row">
+        <div class="col col-lg-12">
+            <div class="page-header">
+                <h1 class="gradient-red">Online Buchen</h1>
+            </div>
         </div>
-      </a>
     </div>
-    <div class="col col-xs-4">
-      <a href="/online-buchen/nur-flug">
-        <div class="box text-center">
-          <i class="fa fa-plane fa-5x gradient"></i>
-          <h1 class="gradient">Flüge</h1>
+    <div class="row">
+        <div class="col col-xs-4">
+            <a href="/online-buchen">
+                <div class="box text-center">
+                    <i class="fa fa-suitcase fa-5x gradient"></i>
+
+                    <h1 class="gradient">Pauschal</h1>
+                </div>
+            </a>
         </div>
-      </a>
-    </div>
-    <div class="col col-xs-4">
-      <a href="/online-buchen/nur-hotel">
-        <div class="box text-center">
-          <i class="fa fa-institution fa-5x gradient"></i>
-          <h1 class="gradient">Hotels</h1>
+        <div class="col col-xs-4">
+            <a href="/online-buchen/nur-flug">
+                <div class="box text-center">
+                    <i class="fa fa-plane fa-5x gradient"></i>
+
+                    <h1 class="gradient">Flüge</h1>
+                </div>
+            </a>
         </div>
-      </a>
+        <div class="col col-xs-4">
+            <a href="/online-buchen/nur-hotel">
+                <div class="box text-center">
+                    <i class="fa fa-institution fa-5x gradient"></i>
+
+                    <h1 class="gradient">Hotels</h1>
+                </div>
+            </a>
+        </div>
     </div>
-  </div>
+    <div class="row">
+        <div class="col col-xs-4">
+            <a href="http://www.fitreisen.de/wellness.html?agenturnr=10205511" target="_blank">
+                <img src="/app/themes/rr-press/assets/img/banner/fti/01-wellnesswochenende/01a_FIT-Reisen_WellnessWE_220Quellenhotel_336x280.gif" class="img-responsive" style="min-width:100%;">
+            </a>
+        </div>
+    </div>
 </div>
